1155f74dc47b6a8894bea16ec4fc852f790a149d
KirylAtrakhimovich <kiryl.atrakhimovich@digitalinspirationlabs.io>
2023-09-19 14:37:29 +0300

    Initial Commit

---
+++ food_recipes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  food_recipes
+//
+//  Created by Kirill Atrakhimovich on 19.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
