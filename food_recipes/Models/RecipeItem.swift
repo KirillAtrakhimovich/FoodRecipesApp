a39c326a1d18660d9e970c98e58e3a3c4849c120
KirylAtrakhimovich <kirill.atrakhimovich@gmail.com>
2024-01-19 14:52:07 +0300

    recoding and fixes

---
+++ food_recipes/Models/RecipeItem.swift
@@ -0,0 +1,15 @@
+//
+//  RecipeModel.swift
+//  food_recipes
+//
+//  Created by Kirill Atrakhimovich on 5.10.23.
+//
+
+import Foundation
+import UIKit
+
+struct RecipeItem {
+    var uri: String
+    var label: String
+    var image: UIImage
+}
---
+++ food_recipes/Views/MainViewController/MainViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  food_recipes
+//
+//  Created by Kirill Atrakhimovich on 19.09.23.
+//
+
+import UIKit
+import Moya
+
+@available(iOS 15.0, *)
+class MainViewController: NiblessViewController, UITableViewDelegate {
+    let provider = MoyaProvider<MoyaExampleService>()
+    var recipes: RecipesNetworkModel?
+    var recipeItems: [RecipeItem]?
+    private let mainView = MainView()
+    
+    override func loadView() {
+        view = mainView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getResponse()
+        mainView.setup()
+        setupTableSettings()
+    }
+    
+    private func setupTableSettings() {
+        mainView.tableView.dataSource = self
+        mainView.tableView.delegate = self
+    }
+    
+    private func getResponse() {
+        provider.request(.getRecipes) { result in
+            switch result {
+            case .success(let moyaResponse):
+                let data = moyaResponse.data
+                self.recipes = try? JSONDecoder().decode(RecipesNetworkModel.self, from: data)
+        
+              
+            case .failure(let error):
+                print(error.errorDescription ?? "Unknown error")
+            }
+        }
+    }
+    
+}
+
+@available(iOS 15.0, *)
+extension MainViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recipes?.hits.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MainViewCell.identifier, for: indexPath) as? MainViewCell else {
+            return UITableViewCell()
+        }
+//        guard let model = model else { return UITableViewCell() }
+//        let item = model.filteredGames[indexPath.row]
+//        cell.fillCell(game: item)
+        if let recipe = recipes?.hits[indexPath.row].recipe {
+                    cell.textLabel?.text = recipe.label
+                    print(recipe.label)
+                
+                }
+        cell.isUserInteractionEnabled = true
+        cell.selectionStyle = .none
+        cell.contentView.isHidden = true
+        return cell
+    }
+}
